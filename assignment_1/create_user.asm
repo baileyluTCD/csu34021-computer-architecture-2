5a3e9ca72fa702498e50517dce4974af128c0552
Luke Bailey <baileylu@tcd.ie>
2025-10-15 11:09:53 +0100

    Get arg count parsing working

--- assignment_1/create_user.asm
+++ assignment_1/create_user.asm
@@ -1,23 +1,81 @@
-; Hello World Program - asmtutor.com
-; Compile with: nasm -f elf helloworld.asm
-; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld.o -o helloworld
-; Run with: ./helloworld
-
         SECTION .data
-msg:
-        db 'Hello World!', 0Ah         ; assign msg variable with your message string
+
+regValue:
+        dd 0
+
+invalidArgsMsg:
+        db "nok: no identifier provided", 10, 0
+        invalidArgsMsgLen equ $ - invalidArgsMsg - 1
+
+        stdin equ 0
+        stdout equ 1
+
+        sys_write equ 4
+        sys_read equ 3
+
+        valid_args_number equ 2        ; binary name counts as first arg
+
+        %macro sys_call 0
+        int 0x80
+        %endmacro
+
+        %macro exit 1
+        mov eax, 1                     ; invoke SYS_EXIT (kernel opcode 1)
+        mov ebx, %1                    ; code to exit with
+        sys_call
+        %endmacro
 
         SECTION .text
+
         global _start
 
+        mov ecx, ecx                   ; Memory address to write from
+        mov edx, edx                   ; Number of bytes to write
+
+; Print a string to stdout
+
+; Args:
+; - ecx: Memory address of string to write out
+; - edx: Length of sequence (bytes) in memory to write
+sprint:
+        push eax
+        push ebx
+
+        mov eax, sys_write             ; invoke SYS_WRITE (kernel opcode 4)
+        mov ebx, stdout                ; File descriptor to write to
+        sys_call
+
+        pop ebx
+        pop eax
+        ret
+
+; Throw an error with a string message
+
+; Args:
+; - ecx: Memory address of string to write out
+; - edx: Length of sequence (bytes) in memory to write
+throw:
+        call sprint
+
+        exit 1
+
 _start:
+        pop ecx                        ; first value on the stack is the number of arguments
 
-        mov edx, 13                    ; number of bytes to write - one for each letter plus 0Ah (line feed character)
-        mov ecx, msg                   ; move the memory address of our message string into ecx
-        mov ebx, 1                     ; write to the STDOUT file
-        mov eax, 4                     ; invoke SYS_WRITE (kernel opcode 4)
-        int 80h
+        cmp ecx, valid_args_number
+        jz has_valid_number_of_args    ; if (arg_count != valid_args_number)
 
-        mov ebx, 0                     ; return 0 status on exit - 'No Errors'
-        mov eax, 1                     ; invoke SYS_EXIT (kernel opcode 1)
-        int 80h
+        mov ecx, invalidArgsMsg
+        mov edx, invalidArgsMsgLen
+        call throw                     ; throw(invalidArgsMsg, invalidArgsMsgLen);
+
+has_valid_number_of_args:
+
+        add ecx, '0'                   ; convert number of args to ascii numeral
+        mov dword [regValue], ecx      ; put the value of the number of args into the memory at the label regValue
+
+        mov ecx, regValue
+        mov edx, 4
+        call sprint                    ; sprint(regValue, 4)
+
+        exit 0
