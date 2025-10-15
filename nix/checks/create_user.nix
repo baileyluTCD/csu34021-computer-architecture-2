d4871ae7894bdfa6f563f56f7789951a64341ade
Luke Bailey <baileylu@tcd.ie>
2025-10-15 12:30:25 +0100

    Auto run any tests under the tests attribute

--- nix/checks/create_user.nix
+++ nix/checks/create_user.nix
@@ -1,6 +1,5 @@
-{ lib, ... }:
-let
-  tests = {
+{
+  tests.createUser = {
     noArgs = ''
       no_args=$(create_user || true)
 
@@ -16,47 +15,4 @@ let
       fi
     '';
   };
-in
-{
-  perSystem =
-    { pkgs, self', ... }:
-    let
-      prelude = ''
-        set -euo pipefail
-
-        mkdir $out
-
-        throw () {
-          if [ $# != 1 ]; then
-            echo "Invalid arg count provided to 'print_error'"
-            echo "Expected one argument"
-          fi
-
-          printf "\033[31mError:\033[0m %s\n" "$1"
-          exit 1
-        }
-      '';
-
-      writeTest =
-        { name, value }:
-        let
-          test = "test-create_user-${name}";
-        in
-        {
-          "${test}" =
-            pkgs.runCommand "${test}"
-              {
-                nativeBuildInputs = [ self'.packages.create_user ];
-              }
-              ''
-                ${prelude}
-                ${value}
-              '';
-        };
-
-      checks = tests |> lib.attrsToList |> map writeTest |> lib.mergeAttrsList;
-    in
-    {
-      inherit checks;
-    };
 }
---
+++ nix/checks/default.nix
@@ -0,0 +1,56 @@
+{ config, lib, ... }:
+let
+  inherit (lib) mkOption types;
+in
+{
+  options.tests = mkOption {
+    description = ''
+      Bash tests to run for this repository's asm packages
+    '';
+    type = types.attrsOf (types.attrsOf types.str);
+  };
+
+  config.perSystem =
+    { pkgs, self', ... }:
+    let
+      prelude = ''
+        set -euo pipefail
+
+        mkdir $out
+
+        throw () {
+          if [ $# != 1 ]; then
+            echo "Invalid arg count provided to 'print_error'"
+            echo "Expected one argument"
+          fi
+
+          printf "\033[31mError:\033[0m %s\n" "$1"
+          exit 1
+        }
+      '';
+
+      writeTests =
+        namespace: tests:
+
+        lib.mapAttrsToList (
+          test: script:
+          let
+            testName = "test-${namespace}-${test}";
+          in
+          {
+            ${testName} =
+              pkgs.runCommand "${testName}"
+                {
+                  nativeBuildInputs = lib.attrValues self'.packages;
+                }
+                ''
+                  ${prelude}
+                  ${script}
+                '';
+          }
+        ) tests;
+    in
+    {
+      checks = config.tests |> lib.mapAttrsToList writeTests |> lib.flatten |> lib.mergeAttrsList;
+    };
+}
